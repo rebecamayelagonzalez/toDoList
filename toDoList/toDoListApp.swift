37a9db997801ff972d38d7772317117183687eac
Rebeca Gonzalez <rebeca.mayela.gzz@gmail.com>
2023-07-18 10:35:59 -0500

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
